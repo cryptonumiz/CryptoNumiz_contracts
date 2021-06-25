726bc4c8e42aa1362cdfd8520faf9d008eab06f9
Oleg Varnov <olegvarnov@gmail.com>
2021-06-25 16:23:15 +0300

    updated Numiz user contract

--- contracts/NumiUser.sol
+++ contracts/NumiUser.sol
@@ -1,65 +1,86 @@
-pragma ton-solidity >= 0.39.0;
+pragma ton-solidity >= 0.46.0;
 pragma AbiHeader expire;
 pragma AbiHeader pubkey;
+pragma AbiHeader time;
 
 import "./interfaces/INumiUser.sol";
+
+interface IRootNumiEvent {
+    function deployEvent(NumiEventDetails newEvent) external;
+}
+
+interface INumiEvent {
+    function deployNumizCollectionRoot(TvmCell m_numizCollectionRootImage) external;
+}
 
 contract NumiUser is INumiUser {
     // Error codes:
     uint constant ERROR_NO_PUBKEY = 101;
-    uint constant ERROR_SENDER_IS_NOT_OWNER = 102;
+    uint constant ERROR_SENDER_IS_NOT_MY_OWNER = 102;
+    uint constant ERROR_SENDER_IS_NOT_MY_ROOT = 103;
+    uint constant ERROR_HAVE_NO_SALT = 104;
 
     uint constant ERROR_USERNAME_IS_TOO_LONG = 200;
     uint constant ERROR_BIO_IS_TOO_LONG = 201;
 
     // State:
-    address static public m_root_address;
     address static public m_owner_address;
     uint256 static m_owner_pubkey;
-    TvmCell static m_code;
+
+    address public m_eventRoot;
 
     string public m_username;
     string public m_bio;
 
+    TvmCell static m_numizCollectionRootCode;
+
     // Constants:
-    uint constant USERNAME_MAX_LENGTH = 60;
-    uint constant BIO_MAX_LENGTH = 500;
+    uint constant USERNAME_MAX_LENGTH = 15;
+    uint constant BIO_MAX_LENGTH = 300;
 
-    constructor(string username, string bio) public {
-        require(tvm.pubkey() != 0, ERROR_NO_PUBKEY);
-        require(msg.sender == m_root_address, ERROR_SENDER_IS_NOT_OWNER);
-        tvm.accept();
-
-        m_username = username;
-        m_bio = bio;
-    }
-
-    modifier onlyOwner {
-        require(msg.pubkey() == m_owner_pubkey, ERROR_SENDER_IS_NOT_OWNER);
+    modifier onlyRoot {
+        require(msg.sender == _calcRoot(), ERROR_SENDER_IS_NOT_MY_ROOT);
         _;
     }
 
+    modifier onlyOwner {
+        require(msg.sender == m_owner_address, ERROR_SENDER_IS_NOT_MY_OWNER);
+        _;
+    }
+
+    constructor(address eventRoot) public onlyRoot {
+        m_eventRoot = eventRoot;
+    }
+
     function setUsername(string _username) external override onlyOwner {
-        require(_username.byteLength() <= USERNAME_MAX_LENGTH, ERROR_USERNAME_IS_TOO_LONG);
-        tvm.accept();
         m_username = _username;
     }
 
     function setBio(string _bio) external override onlyOwner {
-        require(_bio.byteLength() <= BIO_MAX_LENGTH, ERROR_BIO_IS_TOO_LONG);
-        tvm.accept();
         m_bio = _bio;
+    }
+
+    function createEvent(NumiEventDetails newEvent) external override view onlyOwner {
+        IRootNumiEvent(m_eventRoot).deployEvent{value: 0, flag: 64}(newEvent);
+    }
+
+    function createNumizCollectionRoot() external override view onlyOwner {
+        INumiEvent(m_eventRoot).deployNumizCollectionRoot{value: 0, flag: 64}(m_numizCollectionRootImage);
+    }
+
+    function _calcRoot() private inline pure returns (address) {
+        optional(TvmCell) optSalt = tvm.codeSalt(tvm.code());
+        require(optSalt.hasValue(), ERROR_HAVE_NO_SALT);
+        return optSalt.get().toSlice().decode(address);
     }
 
     function getDetails() external override view returns (INumiUserDetails) {
         return INumiUserDetails(
-            m_root_address,
             m_owner_address,
             m_owner_pubkey,
             m_username,
             m_bio,
-            address(this).balance,
-            m_code
+            address(this).balance
         );
     }
 
@@ -74,6 +95,6 @@ contract NumiUser is INumiUser {
 	}
 
     // fallback function
-    fallback() external pure {
+    fallback() external {
     }
 }
--- contracts/interfaces/INumiUser.sol
+++ contracts/interfaces/INumiUser.sol
@@ -1,19 +1,27 @@
-pragma ton-solidity >= 0.39.0;
-pragma AbiHeader expire;
+pragma ton-solidity >= 0.46.0;
+
+struct NumiEventDetails {
+        string title;
+        string description;
+        uint64 startTimestamp;
+        uint64 endTimestamp;
+        address coverImage;
+        address substrateImage;
+    }
 
 interface INumiUser {
     struct INumiUserDetails {
-        address root_address;
         address owner_address;
         uint256 owner_pubkey;
         string username;
         string bio;
         uint128 balance;
-        TvmCell code;
     }
 
     function setUsername(string _username) external;
     function setBio(string _bio) external;
+
+    function createEvent(NumiEventDetails newEvent) external view;
 
     function getDetails() external view returns (INumiUserDetails);
 }
